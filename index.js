7bab6e949a1679bd1a649f28e14338c13783577d
Neyaz Morshid <niyazmorshid@gmail.com>
2025-01-13 18:34:26 +0600

    all done

--- index.js
+++ index.js
@@ -112,6 +112,14 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/plants/:id', async (req, res) => {
+      const id = req.params.id
+      console.log(id)
+      const query = { _id: new ObjectId(id) }
+      const result = await plantsCollection.findOne(query)
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     // await client.db('admin').command({ ping: 1 })
     console.log(
